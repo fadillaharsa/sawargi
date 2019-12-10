7d9e95b7c33755b9854189d17502c38050ba6e80
ariqfmutyara <ariqmutyara@gmail.com>
2019-12-11 02:19:57 +0700

    Updated Kontak

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -5,7 +5,14 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-
+            <p>
+            Kontak kami di :  
+            email     : dukungan.tim@sawargi.co.id
+            telepon   : +6285277055877
+            facebook  : sawargi 
+            twiter    : sawargi
+            instagram : sawargi
+            </p>
 
         </div>
     </div>

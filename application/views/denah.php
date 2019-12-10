8d118d2e017e6a77b97f5f05dd141bf2d26bd66d
radjfauzi <radjfauzi@gmail.com>
2019-12-11 06:35:26 +0700

    pushreq

--- application/views/denah.php
+++ application/views/denah.php
@@ -5,12 +5,13 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-3" style="background-color:#76DAC3; padding:30px">
+            <div class="col-sm-3" style="background-color:#76DAC3; height:100%">
                 <?php include "menu.php"; ?>
-            <div>
-
+            </div>
+				
             <div class="col-sm-9">
-            <div>
+            	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.509039429673!2d107.73894421477937!3d-6.949120969962013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c36b9bba9569%3A0xc4a6dcd562609cbd!2sPerumahan%20Taman%20Cileunyi!5e0!3m2!1sid!2sid!4v1576019081277!5m2!1sid!2sid" width=100% height=100% frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
 
         </div>
     </div>
--- application/views/denah_admin.php
+++ application/views/denah_admin.php
@@ -7,10 +7,16 @@
         <div class="row">
             <div class="col-sm-3" style="background-color:#76DAC3; padding:30px">
                 <?php include "menu.php"; ?>
-            <div>
+            </div>
 
             <div class="col-sm-9">
-            <div>
+            	<div class="form-group">
+            	<form>
+            		<input style="margin-bottom:10px" name="username_lembaga" class="form-control" type = "text" size=40 value="" placeholder="Link Denah">
+            		<button type="submit" style="border:0px; background-color:#5CAEA5; margin-bottom: 15px; border-radius:10px " class="btn btn-primary btn-block">Submit</button>
+            	</form>
+            	</div>
+            </div>
 
         </div>
     </div>

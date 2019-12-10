e58a3e9d3ca0911feb9db66ea1ece10a68d42d6d
radjfauzi <radjfauzi@gmail.com>
2019-12-10 23:54:14 +0700

    loginpage

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,27 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-        
+
+        <div class="col-sm-2"></div>
+        	<div class="col-sm-3" style="margin-top: 200px">
+        		<h1>Sawargi</h1>
+        		<div class="row">
+					<p>Sistem Informasi Warga Interaktif</p>
+        			<div class="col-sm-12" style="background-color:#76DAC3; border-radius:15px; padding:30px">
+			        	<form action="" method="post" enctype="multipart/form-data">					
+			        		<div class="form-group">
+									<input name="username_lembaga" style="border:0px; background-color:#95E9D1; margin-bottom: 15px; border-radius:10px" type = "text" class="form-control"  value="" placeholder="Username">
+									<input name="password_lembaga" style="border:0px;  background-color:#95E9D1; margin-bottom: 15px; border-radius:10px" type = "password" class="form-control"  value="" placeholder="Password">
+							</div>
+								<button type="submit" style="border:0px; background-color:#5CAEA5; margin-bottom: 15px; border-radius:10px " class="btn btn-primary btn-block">MASUK</button>
+								<p>Belum punya akun?</p>
+								<button type="submit" style="border:0px; background-color:#5CAEA5; border-radius:10px" class="btn btn-primary btn-block">BUAT AKUN</button>
+						</form>	
+				</div>
+					</div>
+        	</div>
+        <div class="col-sm-7"></div>
+
         </div>
     </div>
 </body>

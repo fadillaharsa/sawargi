c6859810bdc106aa859dea301a470122ed98dc8e
ariqfmutyara <ariqmutyara@gmail.com>
2019-12-11 02:09:40 +0700

    Updated Tentang

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -5,8 +5,17 @@
 <body>
     <div class="container-fluid">
         <div class="row">
+            <!-- Ini adalah foto -->
+            <h5>Muhammad Fadhillah Arsa</h5>
+            <p>Full Stack Developer</p>
 
+            <!-- Ini adalah foto -->
+            <h5>Muhammad Siradj Al-Fauzi</h5>
+            <p>Front-End Developer</p>
 
+            <!-- Ini adalah foto-->
+            <h5>Muhammad Ariq Farhansyah Mutyara</h5>
+            <p>Copywriter and Back-End Developer</p>
         </div>
     </div>
 </body>

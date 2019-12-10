8fc6729593c7ca64c6b1834a5049452e10d6010b
ariqfmutyara <ariqmutyara@gmail.com>
2019-12-11 02:03:16 +0700

    Updated Ketentuan

--- application/views/ketentuan.php
+++ application/views/ketentuan.php
@@ -6,7 +6,23 @@
     <div class="container-fluid">
         <div class="row">
             <p>
-                
+                Sawargi adalah website untuk mempermudah dan menghubungkan antara warga dengan tetangganya dan juga dengan 
+                tenaga pendidik yang lain. Sawargi dibuat untuk mempermudah pelayanan warga agar warga lebih nyaman dalam
+                mengatur apa - apa tentang tempat tinggalnya.
+            </p>
+            <h6>Perilaku Pengguna</h6>
+            <p>
+                Pengguna harus menjaga etika dan kesopanan saat melakukan diskusi agar suasana diskusi menjadi kondusif dan nyaman. 
+                Pengguna diperbolehkan berdiskusi antar pengguna pada kolom komentar selama masih sesuai dengan tema pembahasan. 
+                Setiap pelanggaran akan mendapatkan sanksi peringatan hingga penghapusan komentar.
+            </p>
+            <h6>Penggunaan Informasi</h6>
+            <p>
+                Penggunaan informasi web sawargi untuk kepentingan umum diperbolehkan dengan syarat melampirkan sumber informasi. 
+                Pengguna dilarang menggunakan informasi web sawargi untuk melakukan perilaku melanggar hukum dan norma. Kiriman di 
+                sawargi dapat merupakan hasil karya penulis sebagaimana tercantum pada bagian akhir kiriman, maupun merupakan salinan 
+                dari berbagai sumber yang mempebolehkan penyebaran ulang informasi. Anda dapat melaporkan segala bentuk kesalahan informasi 
+                yang tersaji melalui halaman Kontak Kami.
             </p>
 
         </div>

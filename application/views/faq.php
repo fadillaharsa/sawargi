2c8593f870f676293d6b6336e49f041010c3578f
ariqfmutyara <ariqmutyara@gmail.com>
2019-12-11 02:15:31 +0700

    Updated FaQ

--- application/views/faq.php
+++ application/views/faq.php
@@ -5,7 +5,8 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-
+            <strong>Q: Apa itu Sawargi</strong>
+            <p>P: Sawargi adalah website sistem pelayanan dan sistem informasi yang menghubungkan warga dengan tenaga pendukung lainnya</p>
         </div>
     </div>
 </body>
